ced0e34627e0c50e88f6236fee48ddd078c09af4
ADSOJAVIERCAMPOS <jc258@gmail.com>
2024-06-05 00:59:56 -0500

    actualización de evidencias

---
+++ Login Pan De Dios/Login Pan de Dios/script.js
@@ -0,0 +1,23 @@
+// Simular BD
+let usuariosRegistrados = [{userEmail: 'admin@admin.com', userPassword: 'admin'}, {userEmail: 'user@user.com', userPassword: 'user'}, {userEmail: 'sena@sena.com', userPassword: 'sena'}, {userEmail: '2627060@2627060.com', userPassword: '2627060'}];
+document.getElementById('formLogin').addEventListener('submit', function (e){
+    e.preventDefault();
+
+    // Obtener los valores usuario y contraseña
+    let userEmail = document.getElementById('userEmail').value;
+    let userPassword = document.getElementById('userPassword').value;
+
+    // Verificar que las credenciales esten en la BD
+    let usuarioValido = usuariosRegistrados.find(function(user){
+        return user.userEmail === userEmail && user.userPassword === userPassword;
+    });
+
+    if(usuarioValido) {
+        window.location.href = 'index.html';
+    } else {
+        let errorMessage = document.getElementById('error-message');
+        errorMessage.textContent = 'Usuario o contraseña incorrecto. Por favor, inténtalo de nuevo.'
+    }
+
+})
+
